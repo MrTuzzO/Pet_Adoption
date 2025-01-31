a09ec6ede9970dc7697b7f580566d16f7b69a25e
MrTuzzO <khirulislam5750@gmail.com>
2025-02-01 00:24:03 +0600

    bug fixed

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
 // const root_api = "https://pet-adoption-omega-seven.vercel.app";
-const root_api = "http://127.0.0.1:8000";
-// const root_api = "https://pet-adoption-drf.onrender.com";
+// const root_api = "http://127.0.0.1:8000";
+const root_api = "https://pet-adoption-drf.onrender.com";
